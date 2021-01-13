97277722ea64d0f6d7b032220b9fa47901f8b473
hulahoo <hulahoo@gmail.com>
2021-01-13 14:20:10 +0600

    Bug fixed in main urls

--- sturdy_guacamole/urls.py
+++ sturdy_guacamole/urls.py
@@ -18,4 +18,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include(router.urls)),
-] + + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
