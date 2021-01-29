baa276f4eb5360aa0a32782a6ce27b1d552497bf
hulahoo <hulahoo@gmail.com>
2021-01-29 14:59:13 +0600

    Changed GDAL

--- sturdy_guacamole/settings.py
+++ sturdy_guacamole/settings.py
@@ -176,8 +176,7 @@ MATERIAL_ADMIN_SITE = {
     }
 }
 
-GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
-GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgdal.so' if os.environ.get('ENV') == 'HEROKU' else os.getenv('GDAL_LIBRARY_PATH')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
