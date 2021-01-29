c566342c604dd56b499e5eee3e60c7f11735ef5c
hulahoo <hulahoo@gmail.com>
2021-01-29 14:54:06 +0600

    Added GDAL_LIBRARY_PATH

--- sturdy_guacamole/settings.py
+++ sturdy_guacamole/settings.py
@@ -176,6 +176,8 @@ MATERIAL_ADMIN_SITE = {
     }
 }
 
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
